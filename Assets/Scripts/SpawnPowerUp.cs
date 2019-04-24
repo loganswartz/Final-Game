77105f1eca06c129389e1b21b1aec53a396637a7
Aaron  George <angeorge@nc12-imac14.gc.goshen.edu>
2019-04-23 20:33:04 -0400

    Add script to spawn powerups

---
+++ Assets/Scripts/SpawnPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPowerUp : MonoBehaviour {
+	
+	public GameObject powerUp;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < this.transform.childCount; i += 2) {
+			var node = this.transform.GetChild(i);
+
+			Vector3 pos = node.transform.position; 
+			GameObject g = (GameObject)Instantiate(powerUp, pos,Quaternion.identity); 
+		}
+	}
+}
